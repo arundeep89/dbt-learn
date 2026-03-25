d4b4b015ab59499fda53fbb22facf467e666e5f6
Arun Pillai <arundeepak89@gmail.com>
2026-03-24 22:40:48 -0400

    feature_1: modify slim CI

--- macros/generate_custom_schema.sql
+++ macros/generate_custom_schema.sql
@@ -3,6 +3,12 @@
     {%- if custom_schema_name is none -%}
         {{ default_schema }}
     {%- else -%}
-        {{ custom_schema_name | trim }}
+        {%- if target.name == 'ci' -%}
+            {# In CI, concatenate base schema with custom schema for PR isolation #}
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+        {%- else -%}
+            {# In prod/dev, use only the custom schema name #}
+            {{ custom_schema_name | trim }}
+        {%- endif -%}
     {%- endif -%}
 {%- endmacro %}
